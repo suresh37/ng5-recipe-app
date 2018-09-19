5f7344946179160b6fbc9b020bec5085cbc22d50
suresh37 <sureshkarthick37@gmail.com>
2018-09-19 22:02:13 +0530

    added inter component communication - input() & output()

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+ @Output() featureSelected = new EventEmitter<string>();
 
+   onSelect(feature: string)
+   {
+      this.featureSelected.emit(feature);
+   }
   constructor() { }
 
   ngOnInit() {
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
     new Recipe("A Test Recipe 1","This is simply a test ","https://c1.staticflickr.com/6/5496/31479301445_cb53c0f4e9_b.jpg"),
@@ -16,6 +17,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  onRecipeSelected(recipe: Recipe){
+    console.log("recipeWasSelected emitted from recipe-list");
+   this.recipeWasSelected.emit(recipe);
   }
 
 }
