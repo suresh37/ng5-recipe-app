06c0d7792af661978ac3532fd5945741bb0bb775
suresh37 <sureshkarthick37@gmail.com>
2018-09-20 22:47:38 +0530

    Add Ingredient to ShoppingList

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  @ViewChild('ingName') ingNameRef: ElementRef;
+  @ViewChild('ingAmt') ingAmtRef: ElementRef;
+  @Output() ingAdded = new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit() {
+  }
+  onIngAdd(){
+    const ingName = this.ingNameRef.nativeElement.value;
+    const ingAmt = this.ingAmtRef.nativeElement.value;
+    const ingredient = new Ingredient(ingName,ingAmt);
+    this.ingAdded.emit(ingredient);
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -14,5 +14,7 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  onIngAdded(ingredient: Ingredient){
+    this.ingredients.push(ingredient);
+  }
 }
